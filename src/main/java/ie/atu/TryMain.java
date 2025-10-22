b41cb31c992d1a024e47bbe76b64866cdf732e31
Oliwier <G00434405@atu.ie>
2025-10-22 13:11:26 +0100

    update

--- src/main/java/ie/atu/TryMain.java
+++ src/main/java/ie/atu/TryMain.java
@@ -6,17 +6,20 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TryMain {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int age;
-       System.out.println("Enter your age");
-       String text = sc.nextLine().trim();
-       try {
-           age = Integer.parseInt(text);
-           System.out.println("Your age is " + age);
-       }
-       catch (NumberFormatException e) {
-           System.out.println("Invalid input" + e.getMessage());
-       }
+        Scanner sc = new Scanner(System.in);
+        int age;
+        while (true) {
 
+            System.out.println("Enter your age");
+            String text = sc.nextLine().trim();
+            try {
+                age = Integer.parseInt(text);
+                System.out.println("Your age is " + age);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input" + e.getMessage());
+            }
+
+        }
     }
 }
