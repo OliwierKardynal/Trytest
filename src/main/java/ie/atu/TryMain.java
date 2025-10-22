917d62154bdc91ce7de4317679d18f20a7229488
Oliwier <G00434405@atu.ie>
2025-10-22 12:17:13 +0100

    Initial Commit. simple program to enter age with try and catch for error handling

---
+++ src/main/java/ie/atu/TryMain.java
@@ -0,0 +1,22 @@
+package ie.atu;
+import java.util.Scanner;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TryMain {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int age;
+       System.out.println("Enter your age");
+       String text = sc.nextLine().trim();
+       try {
+           age = Integer.parseInt(text);
+           System.out.println("Your age is " + age);
+       }
+       catch (NumberFormatException e) {
+           System.out.println("Invalid input" + e.getMessage());
+       }
+
+    }
+}
